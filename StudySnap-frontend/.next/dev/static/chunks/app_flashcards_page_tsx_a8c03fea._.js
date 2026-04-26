32263f4a50292654a110be1cf5786be5a30eede2
Sok Sophorn <sok.sophorn24@kit.edu.kh>
2026-04-26 16:45:20 +0700

    remove accidental StudySnap-frontend folder

--- StudySnap-frontend/.next/dev/static/chunks/app_flashcards_page_tsx_a8c03fea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f974d817._.js",
-  "static/chunks/node_modules_e6ff4057._.js"
-],
-    source: "dynamic"
-});
